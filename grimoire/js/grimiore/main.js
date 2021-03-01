a0e3285dc97bd49dcbb4cf5de9bc2cef88083755
Jordan Carter <jordan.carter.0023@gmail.com>
2021-03-01 10:07:32 -0600

    finished grimoire

--- grimoire/js/grimiore/main.js
+++ grimoire/js/grimiore/main.js
@@ -3,3 +3,50 @@
 console.log("Do you believe in magic?");
 console.log("------------------------");
 
+const spellList = [
+    {
+        Name:"Turn enemy into a newt",
+        IsEvil: true,
+        EnergyReqired: 5.1
+    },
+    {
+        Name: "Conjure some gold for a local charity",
+        IsEvil: false,
+        EnergyReqired: 2.99
+    },
+    {
+        Name: "Give a deaf person the ability to heal",
+        IsEvil: false,
+        EnergyReqired: 12.2
+    },
+    {
+        Name: "Make yourself emperor of the universe",
+        IsEvil: true,
+        EnergyReqired: 100.0
+    },
+    {
+        Name: "Convince your relatives your political views are correct",
+        IsEvil: false,
+        EnergyReqired: 2921.5
+    }
+]
+
+const goodBook = () => {
+    console.log('Good Book')
+    for ( const spell of spellList) {
+        if (spell.IsEvil === false) {
+            console.log(`Spell:${spell.Name} Energy:${spell.EnergyReqired}`)
+        } 
+    }
+}
+const evilBook = () => {
+    console.log('Evil Book')
+    for ( const spell of spellList) {
+        if (spell.IsEvil === true) {
+            console.log(`Spell:${spell.Name} Energy:${spell.EnergyReqired}`)
+        }
+    }
+}
+
+goodBook();
+evilBook();
